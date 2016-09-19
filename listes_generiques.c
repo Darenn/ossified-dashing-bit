b460ca94903965be73a09f8ea2a189e1166e1656
Darenn Keller <keller.darenn@gmail.com>
2016-09-19 15:15:23 +0200

    Documentation de listes_generiques.

--- listes_generiques.c
+++ listes_generiques.c
@@ -4,8 +4,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*! \file
+ * \brief La documentation pour les parties (types, variables, fonctions…)
+ * internes du modules sont documentées ici.
+ *
+ * \copyright PASD
+ * \version 2016
+ */
+
+/*! Un maillon est un pointeur sur (référence vers) une struct maillon_struct .
+ */
 typedef struct maillon_struct *maillon;
 
+/*!
+ * Structure servant à définir un élément du type de la valeur de la liste.
+ * Cette structure n'est visible que depuis listes_generiques.c .
+ * Les fonctions attenantes sont static pour être également masquées. */
 struct maillon_struct {
   void *val;
   maillon suivant;
@@ -18,23 +32,76 @@ struct maillon_struct {
 /* Pour l'essentiel il s'agit de reprendre les fonctions pour les listes */
 /* d'entiers et de les adapter à la généricité */
 
+/*!
+ * Création d'un maillon dont la valeur est val .
+ * suivant et precedent doivent pointer sur ce maillon.
+ * \param val un pointeur vers la valeur à stocker dans le maillon.
+ * \param copier un pointeur vers une fonction qui définit la valeur du maillon
+ * à partir d'une recopie.
+ * \return nouveau maillon stockant val et bouclant sur lui-même.
+ */
 static maillon maillon_creer(void *val, void (*copier)(void *val, void **pt)) {}
 
+/*!
+ * Test pour savoir si le maillon est unique ( bouclant sur lui-même et
+ * correspondant à une liste de taille 1 ).
+ * \param m maillon à tester.
+ */
 static bool maillon_est_unique(maillon m) {}
 
+/*!
+ * Destruction de toute la liste chaînée.
+ * \param m (pointeur sur un) maillon de la liste chaînée à détruire.
+*/
 static void maillon_detruire(maillon *m, void (*detruire)(void **pt)) {}
 
+/*!
+ * Affichage d'un ensemble de maillon à partir d'un début et d'une fin.
+ * Le résultat est de la forme \verbatim[ 1 2 3 ]\endverbatim sans saut de
+ * ligne.
+ * \param f flux où imprimer.
+ * \param m_debut premier maillon à imprimer.
+ * \param m_fin dernier maillon à imprimer.
+ * \param afficher un pointeur vers une fonction qui définit comment on affiche
+ * la valeur du maillon.
+ */
 static void maillon_afficher(FILE *f, maillon m_debut, maillon m_fin,
                              void (*afficher)(FILE *f, void *val)) {}
 
+/*!
+ * Ajout d'un élément avant le maillon.
+ * \param m maillon avant lequel on doit insérer.
+ * \param _val un pointeur vers la valeur entière à insérer.
+ * \param copier un pointeur vers une fonction qui définit la valeur du maillon
+ * à partir d'une recopie.
+ */
 static void maillon_ajouter_avant(maillon m, void *_val,
                                   void (*copier)(void *val, void **pt)) {}
 
+/*!
+ * Ajout d'un élément après le maillon.
+ * \param m maillon après lequel on doit insérer.
+ * \param _val un pointeur vers la valeur entière à insérer.
+ * \param copier un pointeur vers une fonction qui définit la valeur du maillon
+ */
 static void maillon_ajouter_apres(maillon m, void *_val,
                                   void (*copier)(void *val, void **pt)) {}
 
+/*!
+* Suppression du maillon d'avant s'il existe (c.-à-d. si la liste
+* correspondante ne contient qu'un maillon on ne fait rien).
+* \param m maillon avant lequel on doit supprimer.
+* \param copier un pointeur vers une fonction qui définit comment désallouer la
+* mémoire relative à la valeur du maillon
+*/
 static void maillon_supprimer_avant(maillon m, void (*detruire)(void **pt)) {}
 
+/*! suppression du maillon d'après s'il existe (c.-à-d. si la liste
+ * correspondante ne contient qu'un maillon on ne fait rien).
+ * \param m maillon après lequel on doit supprimer.
+ * \param copier un pointeur vers une fonction qui définit comment désallouer la
+ * mémoire relative à la valeur du maillon
+*/
 static void maillon_supprimer_apres(maillon m, void (*detruire)(void **pt)) {}
 
 /* la structure définit 3 champs supplémentaires qui sont des pointeurs sur
